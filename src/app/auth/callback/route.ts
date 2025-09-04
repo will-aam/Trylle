b0f8e9925079caa1fea29ef6b6c6a87e9dda6a22
Will <will00987654@gmail.com>
2025-09-04 13:15:22 -0300

    Melhorias de login e auth

---
+++ src/app/auth/callback/route.ts
@@ -0,0 +1,43 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import { type CookieOptions, createServerClient } from "@supabase/ssr";
+
+export async function GET(request: Request) {
+  const { searchParams, origin } = new URL(request.url);
+  const code = searchParams.get("code");
+  // se houver um 'next' no URL, redirecionamos para lá depois do login
+  const next = searchParams.get("next") ?? "/";
+
+  if (code) {
+    const cookieStore = await cookies();
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          get(name: string) {
+            return cookieStore.get(name)?.value;
+          },
+          set(name: string, value: string, options: CookieOptions) {
+            cookieStore.set({ name, value, ...options });
+          },
+          remove(name: string, options: CookieOptions) {
+            cookieStore.delete({ name, ...options });
+          },
+        },
+      }
+    );
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    if (!error) {
+      return NextResponse.redirect(`${origin}${next}`);
+    }
+  }
+
+  // URL de redirecionamento em caso de erro
+  console.error(
+    "Erro na autenticação: o código de verificação é inválido ou expirou."
+  );
+  return NextResponse.redirect(
+    `${origin}/login?error=Falha na autenticação. Por favor, tente novamente.`
+  );
+}
