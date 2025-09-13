8589a45914d1cc7830aabad836dddabf3e93aca2
Will <will00987654@gmail.com>
2025-09-13 12:03:08 -0300

    Ajuste page ep

--- src/app/admin/episodes/page.tsx
+++ src/app/admin/episodes/page.tsx
@@ -1,11 +1,11 @@
-import { TagManager } from "@/src/components/features/admin/tag-manager/TagManager";
+import { EpisodeManager } from "@/src/components/features/admin/episode-management/episode-manager";
 import { Suspense } from "react";
 
 export default function AdminEpisodesPage() {
   return (
     <div className="container mx-auto px-4 py-8">
       <Suspense fallback={<p>Carregando gerenciador de episódios...</p>}>
-        <TagManager />
+        <EpisodeManager />
       </Suspense>
     </div>
   );
