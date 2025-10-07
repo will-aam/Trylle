b7bd54d0e27010201c81a770b39c3ec27308422b
Will <will00987654@gmail.com>
2025-10-07 20:28:48 -0300

    Removendo imagem de episode card

--- src/components/features/episode-card.tsx
+++ src/components/features/episode-card.tsx
@@ -20,21 +20,9 @@ export function EpisodeCard({ episode }: EpisodeCardProps) {
     }
   };
 
-  // Agora isso vai funcionar, e com a sua imagem de fallback.
-  const imageUrl =
-    episode.programs?.cover_image_path ||
-    "https://rounder.pics/assets/img/ui/square-image.webp";
-
   return (
     <div className="flex flex-col gap-3">
       <div className="relative">
-        <Image
-          src={imageUrl}
-          alt={episode.title}
-          width={300}
-          height={168}
-          className="rounded-xl aspect-video object-cover w-full"
-        />
         {episode.categories && (
           <Badge className="absolute top-3 left-3">
             {episode.categories.name}
